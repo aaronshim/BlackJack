f0bd796943f56298c6e4781999fa6a470116ee84
Aaron Shim <aaron.k.shim@gmail.com>
2017-02-11 15:59:52 -0800

    types

--- Main.hs
+++ Main.hs
@@ -1,3 +1,12 @@
 
+import Data.Set
+
+data Suit = Heart | Spade | Club | Diamond
+type Card = (Int, Suit)
+type Deck = Set Card
+type Hand = [Card]
+data GameState = GameState (Hand) (Hand) (Deck)
+
+main :: IO ()
 main = do
   putStrLn "Hello, world!"
