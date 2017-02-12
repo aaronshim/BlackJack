3eb4ba7a4da8ef3248389c74590c7f59f0bb89cd
Aaron Shim <aaron.k.shim@gmail.com>
2017-02-11 16:32:06 -0800

    bo

--- Main.hs
+++ Main.hs
@@ -1,11 +1,14 @@
 
-import Data.Set
+import qualified Data.Set as S
 
-data Suit = Heart | Spade | Club | Diamond deriving Show
-type Card = (Int, Suit)
-type Deck = Set Card
+data Suit = Heart | Spade | Club | Diamond deriving (Show, Ord, Eq)
+type Card = (Integer, Suit)
+type Deck = S.Set Card
 type Hand = [Card]
-data GameState = GameState (Hand) (Hand) (Deck) deriving Show
+data GameState = GameState { myHand :: Hand
+                           , houseHand :: Hand
+                           , deck :: Deck
+                           } deriving Show
 
 --drawFromDeck :: Deck -> IO (Card, Deck)
 
@@ -17,6 +20,22 @@ data GameState = GameState (Hand) (Hand) (Deck) deriving Show
 
 --gameFinished :: State -> Bool
 
+genInitialState :: GameState
+genInitialState = GameState [] [] genInitialDeck
+  where
+    genInitialDeck = S.fromList cardsList
+    cardsList = concat $ map (\c -> map (\n -> (n, c)) [2..14]) [Heart, Spade, Club, Diamond]
+
 main :: IO ()
 main = do
   putStrLn "Hello, world!"
+  let initialState = genInitialState
+  programLoop initialState
+
+programLoop :: GameState -> IO ()
+programLoop state = do
+  putStrLn $ "Current state: " ++ (show state)
+  -- this is where we do our game calculations
+  let state' = state
+  -- then decide to continue or finish
+  programLoop state
