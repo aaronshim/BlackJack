383348f515e09a45e9822e5fc3bad5c1e8c6491a
Aaron Shim <aaron.k.shim@gmail.com>
2017-02-11 16:07:31 -0800

    functions

--- Main.hs
+++ Main.hs
@@ -7,6 +7,16 @@ type Deck = Set Card
 type Hand = [Card]
 data GameState = GameState (Hand) (Hand) (Deck) deriving Show
 
+--drawFromDeck :: Deck -> IO (Card, Deck)
+
+--addToHand :: Hand -> Card -> Hand
+
+--hasWon :: Hand -> Bool
+
+--hasLost :: Hand -> Bool
+
+--gameFinished :: State -> Bool
+
 main :: IO ()
 main = do
   putStrLn "Hello, world!"
