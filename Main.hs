f322aa1e899e293a01320ff54a6f8cfb735abc9a
Aaron Shim <aaron.k.shim@gmail.com>
2017-02-11 16:01:16 -0800

    deriving Show for custom types

--- Main.hs
+++ Main.hs
@@ -1,11 +1,11 @@
 
 import Data.Set
 
-data Suit = Heart | Spade | Club | Diamond
+data Suit = Heart | Spade | Club | Diamond deriving Show
 type Card = (Int, Suit)
 type Deck = Set Card
 type Hand = [Card]
-data GameState = GameState (Hand) (Hand) (Deck)
+data GameState = GameState (Hand) (Hand) (Deck) deriving Show
 
 main :: IO ()
 main = do
